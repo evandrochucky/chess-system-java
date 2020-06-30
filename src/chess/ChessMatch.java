b0d493a638c1f3d5503adbf94e27667c66deef85
EvandroChucky <evandrochucky@gmail.com>
2020-06-29 23:05:47 -0300

    Handling captured pieces-including ChessMatch.java

--- src/chess/ChessMatch.java
+++ src/chess/ChessMatch.java
@@ -1,5 +1,8 @@
 package chess;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import boardgame.Board;
 import boardgame.Piece;
 import boardgame.Position;
@@ -11,6 +14,9 @@ public class ChessMatch {
 	private int turn;
 	private Color currentPlayer;
 	private Board board;
+	
+	private List<Piece> piecesOnTheBoard = new ArrayList<>();
+	private List<Piece> capturedPieces = new ArrayList<>();
 	
 	public ChessMatch() {
 		// aqui se define o tamanho do tabuleiro do xadrez
@@ -63,6 +69,10 @@ public class ChessMatch {
 		Piece p = board.removePiece(source);
 		Piece capturedPiece = board.removePiece(target);
 		board.placePiece(p, target);
+		if (capturedPiece != null) {
+			piecesOnTheBoard.remove(capturedPiece);
+			capturedPieces.add(capturedPiece);
+		}
 		return capturedPiece;
 	}
 	
@@ -93,6 +103,7 @@ public class ChessMatch {
 	//Instanciar as peças do xadrez informando as coordenadas do sistema do Xadrez
 	private void placeNewPiece(char column, int row, ChessPiece piece) {
 		board.placePiece(piece, new ChessPosition(column, row).toPosition());	
+		piecesOnTheBoard.add(piece);
 	}
 	
 	
